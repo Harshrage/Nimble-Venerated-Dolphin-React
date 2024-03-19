76665efef3d1986a43904d993a087e4d32ed0410
Harshrage <129183199+Harshrage@users.noreply.github.com>
2024-03-19 10:07:48 +0530

    Commit from TeleportHQ

---
+++ src/views/home.js
@@ -0,0 +1,326 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Nimble Venerated Dolphin</title>
+        <meta property="og:title" content="Nimble Venerated Dolphin" />
+      </Helmet>
+      <div className="home-header">
+        <header
+          data-thq="thq-navbar"
+          className="navbarContainer home-navbar-interactive"
+        >
+          <span className="logo">HARSH EDITOR</span>
+          <div data-thq="thq-navbar-nav" className="home-desktop-menu">
+            <nav className="home-links">
+              <span className="bodySmall">Home</span>
+              <span className="home-nav32 bodySmall">About</span>
+              <span className="home-nav42 bodySmall">Contact</span>
+            </nav>
+            <div className="home-buttons"></div>
+          </div>
+          <div data-thq="thq-burger-menu" className="home-burger-menu">
+            <svg viewBox="0 0 1024 1024" className="home-icon socialIcons">
+              <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+            </svg>
+          </div>
+          <div
+            data-thq="thq-mobile-menu"
+            className="home-mobile-menu1 mobileMenu"
+          >
+            <div className="home-nav">
+              <div className="home-top">
+                <span className="logo">PHOTOGRAPHY</span>
+                <div data-thq="thq-close-menu" className="home-close-menu">
+                  <svg
+                    viewBox="0 0 1024 1024"
+                    className="home-icon02 socialIcons"
+                  >
+                    <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                  </svg>
+                </div>
+              </div>
+              <nav className="home-links1">
+                <span className="home-nav121 bodySmall">Home</span>
+                <span className="home-nav22 bodySmall">Portfolio</span>
+                <span className="home-nav321 bodySmall">About</span>
+                <span className="home-nav421 bodySmall">Contact</span>
+              </nav>
+              <div className="home-buttons1">
+                <button className="buttonFlat">Login</button>
+                <button className="buttonFilled">Register</button>
+              </div>
+            </div>
+            <div>
+              <svg
+                viewBox="0 0 950.8571428571428 1024"
+                className="home-icon04 socialIcons"
+              >
+                <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 877.7142857142857 1024"
+                className="home-icon06 socialIcons"
+              >
+                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 602.2582857142856 1024"
+                className="home-icon08 socialIcons"
+              >
+                <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+              </svg>
+            </div>
+          </div>
+        </header>
+      </div>
+      <div className="home-hero">
+        <div className="heroContainer home-hero1">
+          <div className="home-container01">
+            <h1 className="home-hero-heading">MEMORIES OF THE CLASS</h1>
+            <span className="home-hero-sub-heading bodyLarge">
+              <span>
+                <span>
+                  <span>Discover the beauty within</span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                </span>
+                <span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                </span>
+              </span>
+              <span>
+                <span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                </span>
+                <span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                  <span>
+                    <span
+                      dangerouslySetInnerHTML={{
+                        __html: ' ',
+                      }}
+                    />
+                  </span>
+                </span>
+              </span>
+            </span>
+            <div className="home-btn-group">
+              <button className="buttonFlat">Learn More →</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home-features">
+        <div className="home-features-container featuresContainer"></div>
+      </div>
+      <div className="home-pricing">
+        <div className="pricingContainer">
+          <div className="home-container02">
+            <span className="overline">
+              <span>Pricing</span>
+              <br></br>
+            </span>
+            <h2 className="heading2">Choose Your Plan</h2>
+            <span className="home-pricing-sub-heading bodyLarge">
+              <span>
+                <span>Select the perfect plan that suits your needs</span>
+              </span>
+            </span>
+          </div>
+          <div className="home-container03">
+            <div className="freePricingCard home-pricing-card">
+              <div className="home-container04">
+                <span className="home-text19 heading3">Free</span>
+                <span className="bodySmall">
+                  Access to a limited selection of photos
+                </span>
+              </div>
+              <div className="home-container05">
+                <span className="home-text20">₹</span>
+                <span className="home-free-plan-price">50</span>
+              </div>
+              <div className="home-container06">
+                <div className="home-container07">
+                  <span className="home-text21">✔</span>
+                  <span>UPTO 10 PHOTOS </span>
+                </div>
+                <div className="home-container08">
+                  <span className="home-text22">✔</span>
+                  <span>BASIC SHOW CASE </span>
+                </div>
+                <div className="home-container09">
+                  <span className="home-text23">✔</span>
+                  <span className="bodySmall">Limited customer support</span>
+                </div>
+                <div className="home-container10">
+                  <span className="home-text24">✔</span>
+                  <span>WATERMARK </span>
+                </div>
+              </div>
+              <button className="home-button buttonOutline">
+                Continue with Free
+              </button>
+            </div>
+            <div className="basicPricingCard home-pricing-card1">
+              <div className="home-container11">
+                <span className="home-text25 heading3">BASIC</span>
+                <span className="bodySmall">
+                  Ideal for photography enthusiasts
+                </span>
+              </div>
+              <div className="home-container12">
+                <span className="home-text26">₹</span>
+                <span className="home-basic-plan-pricing">150</span>
+              </div>
+              <div className="home-container13">
+                <div className="home-container14">
+                  <span className="home-text27">✔</span>
+                  <span className="bodySmall">All features of FREE plan</span>
+                </div>
+                <div className="home-container15">
+                  <span className="home-text29">✔</span>
+                  <span>
+                    <span>Upto 50 Photos</span>
+                    <br></br>
+                  </span>
+                </div>
+                <div className="home-container16">
+                  <span className="home-text32">✔</span>
+                  <span>
+                    <span>Upto 24 hours type change option</span>
+                    <br></br>
+                  </span>
+                </div>
+                <div className="home-container17">
+                  <span className="home-text35">✔</span>
+                  <span>Watermark</span>
+                </div>
+              </div>
+              <button className="home-button1 buttonFilledSecondary">
+                Try the Basic plan
+              </button>
+            </div>
+            <div className="proPricingCard home-pricing-card2">
+              <div className="home-container18">
+                <span className="home-text36 heading3">
+                  <span>PRO</span>
+                  <br></br>
+                </span>
+                <span className="bodySmall">
+                  For professional photographers and businesses
+                </span>
+              </div>
+              <div className="home-container19">
+                <span className="home-text39">₹</span>
+                <span className="home-pro-plan-pricing">250</span>
+              </div>
+              <div className="home-container20">
+                <div className="home-container21">
+                  <span className="home-text40">✔</span>
+                  <span className="bodySmall"> All features of BASIC plan</span>
+                </div>
+                <div className="home-container22">
+                  <span className="home-text42">✔</span>
+                  <span className="bodySmall">
+                    Unlimited high-resolution photo downloads
+                  </span>
+                </div>
+                <div className="home-container23">
+                  <span className="home-text43">✔</span>
+                  <span>Advanced showcase (any site type)</span>
+                </div>
+                <div className="home-container24">
+                  <span className="home-text44">✔</span>
+                  <span>7 Days Change Option</span>
+                </div>
+                <div className="home-container25">
+                  <span className="home-text45">X</span>
+                  <span>Watermark</span>
+                </div>
+              </div>
+              <button className="home-button2 buttonFilledSecondary">
+                Try the PRO plan
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home-banner"></div>
+      <div className="home-faq">
+        <div className="home-faq-container faqContainer"></div>
+      </div>
+      <div className="home-footer">
+        <footer className="footerContainer home-footer1">
+          <div className="home-container26">
+            <span className="logo">PHOTOGRAPHY</span>
+            <nav className="home-nav1">
+              <span className="bodySmall">Home</span>
+              <span className="home-nav322 bodySmall">About</span>
+              <span className="home-nav422 bodySmall">Contact</span>
+            </nav>
+          </div>
+          <div className="home-separator"></div>
+          <div className="home-container27">
+            <span className="bodySmall home-text46">
+              © 2023 HARSHDEEP, All Rights Reserved.
+            </span>
+            <div className="home-icon-group1">
+              <svg
+                viewBox="0 0 877.7142857142857 1024"
+                className="home-icon10 socialIcons"
+              >
+                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+              </svg>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default Home
